c69ba36864d94c0a90c68a7e76f37674ecc4022a
siddagouniBhavya <bhavyagoudsiddagouni@gmail.com>
2026-06-14 17:15:33 +0530

    Connect frontend to deploy backend

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const apiClient=axios.create({
-    baseURL:"http://localhost:3000/api",
+    baseURL:"https://farmkart-backend.onrender.com",
     withCredentials:true
 });
 
