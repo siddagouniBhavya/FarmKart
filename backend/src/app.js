e13ed35ec3c8f387e941ee575aff8756b2d928b3
siddagouniBhavya <bhavyagoudsiddagouni@gmail.com>
2026-06-14 17:49:26 +0530

    fix CORS and API Url

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,7 +15,7 @@ connectDb()
 const app=express()
 app.use(express.json())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://farm-kart-self.vercel.app/",
     credentials:true
 }))
 
